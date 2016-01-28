491a6b62656f2d554bf66c5d1ce6f6c5b14f698c
Kelvin Del Monte <kdelmonten@gmail.com>
2016-01-28 10:05:15 -0500

    Create file directory for the user if it does not exist

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 /* jshint node:true */
 
 var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
 
 var defaultFilename = 'jshint-output.log';
 
 var wrStream;
 var filename;
 
-module.exports = function (results, data, opts) {
+module.exports = function(results, data, opts) {
   opts = opts || {};
   opts.filename = opts.filename || defaultFilename;
 
@@ -17,6 +19,8 @@ module.exports = function (results, data, opts) {
   }
 
   if (!wrStream) {
+    var directoryPath = path.dirname(opts.filename);
+    mkdirp.sync(directoryPath);
     wrStream = fs.createWriteStream(opts.filename);
     filename = opts.filename;
   }
@@ -26,9 +30,9 @@ module.exports = function (results, data, opts) {
 
 // https://github.com/jshint/jshint/blob/master/src/reporters/jslint_xml.js
 function jslint_xml(results) {
-    "use strict";
+  "use strict";
 
-    var files = {},
+  var files = {},
     out = [],
     pairs = {
       "&": "&amp;",
@@ -39,42 +43,42 @@ function jslint_xml(results) {
     },
     file, i, issue;
 
-    function encode(s) {
-      for (var r in pairs) {
-        if (typeof(s) !== "undefined") {
-          s = s.replace(new RegExp(r, "g"), pairs[r]);
-        }
+  function encode(s) {
+    for (var r in pairs) {
+      if (typeof(s) !== "undefined") {
+        s = s.replace(new RegExp(r, "g"), pairs[r]);
       }
-      return s || "";
     }
-
-
-    results.forEach(function(result) {
-      result.file = result.file.replace(/^\.\//, '');
-      if (!files[result.file]) {
-        files[result.file] = [];
-      }
-      files[result.file].push(result.error);
-    });
-
-    out.push("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
-    out.push("<jslint>");
-
-    for (file in files) {
-      out.push("\t<file name=\"" + file + "\">");
-      for (i = 0; i < files[file].length; i++) {
-        issue = files[file][i];
-        out.push("\t\t<issue line=\"" + issue.line +
-          "\" char=\"" + issue.character +
-          "\" reason=\"" + encode(issue.reason) +
-          "\" evidence=\"" + encode(issue.evidence) +
-          (issue.code ? "\" severity=\"" + encode(issue.code.charAt(0)) : "") +
-          "\" />");
-      }
-      out.push("\t</file>");
-    }
-
-    out.push("</jslint>");
-
-    return out.join("\n") + "\n";
+    return s || "";
   }
+
+
+  results.forEach(function(result) {
+    result.file = result.file.replace(/^\.\//, '');
+    if (!files[result.file]) {
+      files[result.file] = [];
+    }
+    files[result.file].push(result.error);
+  });
+
+  out.push("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+  out.push("<jslint>");
+
+  for (file in files) {
+    out.push("\t<file name=\"" + file + "\">");
+    for (i = 0; i < files[file].length; i++) {
+      issue = files[file][i];
+      out.push("\t\t<issue line=\"" + issue.line +
+        "\" char=\"" + issue.character +
+        "\" reason=\"" + encode(issue.reason) +
+        "\" evidence=\"" + encode(issue.evidence) +
+        (issue.code ? "\" severity=\"" + encode(issue.code.charAt(0)) : "") +
+        "\" />");
+    }
+    out.push("\t</file>");
+  }
+
+  out.push("</jslint>");
+
+  return out.join("\n") + "\n";
+}
